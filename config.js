768dc2a64a48cdcb52764da6db9c09a3f009cfac
CYBER DEXTER <152212325+darkewing@users.noreply.github.com>
2024-03-31 11:07:19 +0530

    Update config.js

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ global.antitags = true;
 
 //auto functioner
 global.autoTyping = false;                //auto tying by default off.
-global.autoRecord = false;                //auto recording by default off.
+global.autoRecord = true;                //auto recording by default off.
 global.groupevent = false;                //This is the new variable for controlling group event handling.
 global.statusseen = true;                 //This is the new variable for controlling status seen.
 global.autoreadgc = true;
@@ -21,10 +21,10 @@ global.prefa = ["."];                                    //Default prefix here. 
 
 
 //
-global.Owner = ["916297175943", "919362639362"];         //If you want singal number so global.Owner = ['916297175943'] Change into your number.
-global.OwnerNumber = ["916297175943", "919362639362"];   //If you want singal number so global.Owner = ['916297175943'] Change into your number.
+global.Owner = ["94785274495"];         //If you want singal number so global.Owner = ['916297175943'] Change into your number.
+global.OwnerNumber = ["94785274495"];   //If you want singal number so global.Owner = ['916297175943'] Change into your number.
 global.ownertag = ["916297175943"];
-global.OwnerName = "Kai";
+global.OwnerName = "CYBERDEXTER";
 global.BotName = "A17";
 global.packname = "A17 Bot";                             //Do not change.
 global.author = "By: Kai";                               //Do not change.
